cece5e5431bd3edda542ba92969509fb3bbc3c30
Pablo Dos Santos <pablo.dossantos@ibisa.co>
2022-10-11 23:37:30 -0500

    primeros pasos

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const { application } = require('express');
+const express = require('express');
+
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+
+
+// Global Variables
+
+
+// Routes
+
+
+// Static Files
+
+
+// Server is Listenning
+app.listen(app.get('port'), )
