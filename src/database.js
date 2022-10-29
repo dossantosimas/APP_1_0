7417918c0304f2af277d49b7c21e54e2136518d0
Robert Vergara <robertver0306@gmail.com>
2022-10-29 12:37:05 -0500

    organizando el frontend sin el servidor

--- src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1/app-torneos-basket', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.error(err));
--- src/routes/equipo.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-router.get('/equipo-formulario', (req, res) => {
-    res.send('Formulario de equipos')
-});
-
-module.exports = router;
