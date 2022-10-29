0fdba82ae63649d90cb27eb1aabd8607e0e4db1d
Robert Vergara <robertver0306@gmail.com>
2022-10-29 11:28:57 -0500

    asdasd

--- src/navigation.js
+++ src/navigation.js
@@ -10,7 +10,16 @@ const navigator = ()=>{
     if(location.hash.startsWith('#segunda')){
         segundaChampionShip();
     }
-    else if(location.hash.startsWith('#home')) {
+    if(location.hash.startsWith('#register')){
+        registro();
+    }
+    if(location.hash.startsWith('#team')){
+        clubes();
+    }
+    if(location.hash.startsWith('#club')){
+        viewClub();
+    }
+    else if(location.hash.startsWith('#home' || '')) {
         homePage();
     }
 }
@@ -18,26 +27,97 @@ const navigator = ()=>{
 const homePage = ()=>{
     console.log('home');
 
+    formCreateTeam.classList.add('ocultar');
+    mainTables.classList.add('ocultar');
+    scorersContainer.classList.add('ocultar');
+    triplesContainer.classList.add('ocultar');
+    HomePage.classList.remove('ocultar');
+    newsContainer.classList.remove('ocultar');
+    newsContainer.classList.add('card-group');
+    clubsContainer.classList.add('ocultar');
+    clubsContainer.classList.remove('row');
+    clubDetailedContainer.classList.add('ocultar');
+    clubDetailedContainer.classList.remove('row');
+
+    leadershipBoardPreview();  
+
+}
+
+const registro = ()=>{
+    console.log('register');
+    
     formCreateTeam.classList.remove('ocultar');
     mainTables.classList.add('ocultar');
     scorersContainer.classList.add('ocultar');
-    triplesContainer.classList.add('ocultar');  
+    triplesContainer.classList.add('ocultar');
+    HomePage.classList.add('ocultar');
+    newsContainer.classList.add('ocultar');
+    newsContainer.classList.remove('card-group');
+    clubsContainer.classList.add('ocultar');
+    clubsContainer.classList.remove('row');
+    clubDetailedContainer.classList.add('ocultar');
+    clubDetailedContainer.classList.remove('row'); 
+
 }
 
 const laLigaChampionShip = ()=>{
     console.log('la Liga');
-
+    HomePage.classList.add('ocultar');
     formCreateTeam.classList.add('ocultar');
     mainTables.classList.remove('ocultar');
     scorersContainer.classList.remove('ocultar');
     triplesContainer.classList.remove('ocultar'); 
+    newsContainer.classList.remove('ocultar');
+    newsContainer.classList.add('card-group');
+    clubsContainer.classList.add('ocultar');
+    clubsContainer.classList.remove('row');
+    clubDetailedContainer.classList.add('ocultar');
+    clubDetailedContainer.classList.remove('row');
 
-    renderLeadershipBoard();
+
+    renderDetailedLeadershipBoard();
     renderAnnotationsList();
     renderTriplesList();
 
 }
 
+const clubes = ()=>{
+    console.log('clubes');
+    HomePage.classList.add('ocultar');
+    formCreateTeam.classList.add('ocultar');
+    mainTables.classList.add('ocultar');
+    scorersContainer.classList.add('ocultar');
+    triplesContainer.classList.add('ocultar'); 
+    newsContainer.classList.add('ocultar');
+    newsContainer.classList.remove('card-group');
+    clubsContainer.classList.remove('ocultar');
+    clubsContainer.classList.add('row');
+    clubDetailedContainer.classList.add('ocultar');
+    clubDetailedContainer.classList.remove('row');
+
+    renderClubs();
+}
+
+const viewClub = ()=>{
+    console.log(`Este es la vista de detalle de un club`);
+    console.log('clubes');
+    HomePage.classList.add('ocultar');
+    formCreateTeam.classList.add('ocultar');
+    mainTables.classList.add('ocultar');
+    scorersContainer.classList.add('ocultar');
+    triplesContainer.classList.add('ocultar'); 
+    newsContainer.classList.add('ocultar');
+    newsContainer.classList.remove('card-group');
+    clubsContainer.classList.add('ocultar');
+    clubsContainer.classList.remove('row');
+    clubDetailedContainer.classList.add('row');
+    clubDetailedContainer.classList.remove('ocultar');
+
+
+    //['#team', 'buscado']
+    const [ , query] = location.hash.split('=');
+    getTeamByQuery(query);
+}
 
 
 window.addEventListener('DOMContentLoaded', navigator, false);
